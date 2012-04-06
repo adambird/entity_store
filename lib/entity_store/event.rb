57a5136666e1bebb538387bc0d3cd132f12e835b
Adam Bird <adam.bird@gmail.com>
2012-04-06 11:42:49 +0100

    added value attribute method to Events class to allow deserialising of value objects as event attributes

--- lib/entity_store/event.rb
+++ lib/entity_store/event.rb
@@ -43,6 +43,12 @@ module EntityStore
         end
       end
       
+      def entity_value_attribute(name, klass)
+        define_method(name) { instance_variable_get("@#{name}") }
+        define_method("#{name}=") do |value|
+          instance_variable_set("@#{name}", value.is_a?(Hash) ? klass.new(value) : value)
+        end
+      end
     end
   end
 end
--- spec/entity_store/event_spec.rb
+++ spec/entity_store/event_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
+class DummyValue
+  include EntityValue
+  attr_accessor :town, :county
+
+end
+
 class DummyEvent
   include Event
   attr_accessor :name
   time_attribute :updated_at, :sent_at
-  
+  entity_value_attribute :address, DummyValue
 end
 
 describe Event do
@@ -19,7 +25,7 @@ describe Event do
     subject { @event.attributes }
     
     it "returns a hash of the attributes" do
-      subject.should eq({:entity_id => @id, :name => @name, :updated_at => @time, :sent_at => nil})
+      subject.should eq({:entity_id => @id, :name => @name, :updated_at => @time, :sent_at => nil, :address => nil})
     end
   end
   
@@ -42,6 +48,36 @@ describe Event do
       it "parses the time field when added as a string" do
         subject
         @event.updated_at.to_i.should eq(@time.to_i)
+      end
+    end
+  end
+  
+  describe ".value_attribute" do
+    before(:each) do
+      @event = DummyEvent.new
+    end
+    context "assign a value" do
+      before(:each) do
+        @value = DummyValue.new(:town => random_string, :county => random_string)
+        @event.address = @value
+      end
+      it "assigns town" do
+        @event.address.town.should eq(@value.town)
+      end
+      it "assigns county" do
+        @event.address.county.should eq(@value.county)
+      end
+    end
+    context "assign a hash" do
+      before(:each) do
+        @hash = { 'town' => random_string, 'county' => random_string }
+        @event.address = @hash
+      end
+      it "assigns town" do
+        @event.address.town.should eq(@hash['town'])
+      end
+      it "assigns county" do
+        @event.address.county.should eq(@hash['county'])
       end
     end
   end
