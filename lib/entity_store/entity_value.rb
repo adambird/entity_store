8dd9f25af23f54f26d833d30bc3ab657752b76b1
Adam Bird <adam.bird@gmail.com>
2012-06-27 12:51:02 +0100

    added respond_to check when initialising entity_values

--- lib/entity_store/entity_value.rb
+++ lib/entity_store/entity_value.rb
@@ -5,25 +5,25 @@ module EntityStore
         extend ClassMethods
       end
     end
-    
+
     def initialize(attr={})
-      attr.each_pair { |k,v| self.send("#{k}=", v) }
+      attr.each_pair { |k,v| self.send("#{k}=", v) if self.respond_to? "#{k}=" }
     end
-    
+
     def attributes
       Hash[*public_methods.select {|m| m =~ /\w\=$/}.collect do |m|
         attribute_name = m.to_s.chop.to_sym
         [attribute_name, send(attribute_name).respond_to?(:attributes) ? send(attribute_name).attributes : send(attribute_name)]
       end.flatten]
     end
-    
+
     def ==(other)
       attributes.each_key do |attr|
         return false unless other.respond_to?(attr) && send(attr) == other.send(attr)
       end
       return true
     end
-    
+
     module ClassMethods
       def entity_value_attribute(name, klass)
         define_method(name) { instance_variable_get("@#{name}") }
