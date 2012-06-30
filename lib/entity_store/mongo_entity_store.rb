28b2a2a04af2e20b525a59a72090cc4235a2236d
Adam Bird <adam.bird@gmail.com>
2012-06-30 15:27:10 +0100

    added connect timeout

--- lib/entity_store/mongo_entity_store.rb
+++ lib/entity_store/mongo_entity_store.rb
@@ -8,40 +8,45 @@ module EntityStore
     def open_connection
       @db ||= open_store
     end
-    
+
     def open_store
       uri  = URI.parse(EntityStore.connection_profile)
-      Connection.from_uri(EntityStore.connection_profile).db(uri.path.gsub(/^\//, ''))
+      connection = Connection.from_uri(EntityStore.connection_profile, :connecttimeoutms => connect_timeout)
+      connection.db(uri.path.gsub(/^\//, ''))
     end
-    
+
+    def connect_timeout
+      ENV['ENTITY_STORE_CONNECT_TIMEOUT'] || '2000'
+    end
+
     def entities
       @entities_collection ||= open_connection['entities']
     end
-    
+
     def events
       @events_collection ||= open_connection['entity_events']
     end
-    
+
     def ensure_indexes
-      events_collection.ensure_index([['entity_id', Mongo::ASCENDING], ['_id', Mongo::ASCENDING]])      
+      events_collection.ensure_index([['entity_id', Mongo::ASCENDING], ['_id', Mongo::ASCENDING]])
     end
-    
+
     def add_entity(entity)
       entities.insert('_type' => entity.class.name, 'version' => entity.version).to_s
     end
-    
+
     def save_entity(entity)
       entities.update({'_id' => BSON::ObjectId.from_string(entity.id)}, { '$set' => { 'version' => entity.version } })
     end
-    
+
     def add_event(event)
       events.insert({'_type' => event.class.name, '_entity_id' => BSON::ObjectId.from_string(event.entity_id) }.merge(event.attributes) ).to_s
     end
-    
+
     def get_entity!(id)
       get_entity(id, true)
     end
-    
+
     def get_entity(id, raise_exception=false)
       begin
         if attrs = entities.find('_id' => BSON::ObjectId.from_string(id)).first
@@ -63,7 +68,7 @@ module EntityStore
     end
 
     def get_events(id)
-      events.find('_entity_id' => BSON::ObjectId.from_string(id)).collect do |attrs| 
+      events.find('_entity_id' => BSON::ObjectId.from_string(id)).collect do |attrs|
         begin
           attrs['_type'].constantize.new(attrs)
         rescue => e
