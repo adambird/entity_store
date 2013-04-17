f88531574882ccbcd8aa04e9913c13bc4b38fa3c
Adam Bird <adam.bird@gmail.com>
2013-04-17 10:26:26 +0100

    added integration test confirming working of event_subscriber publications

--- spec/entity_store_spec.rb
+++ spec/entity_store_spec.rb
@@ -1,1 +1,55 @@
 require 'spec_helper'
+
+class DummyEntity
+  include Entity
+
+  attr_accessor :name
+
+  def set_name(name)
+    record_event DummyEntityNameSet.new(name: name)
+  end
+end
+
+class DummyEntityNameSet
+  include Event
+
+  attr_accessor :name
+
+  def apply(entity)
+    entity.name = name
+  end
+end
+
+class DummyEntitySubscriber
+  class << self
+    attr_accessor :event_name
+  end
+
+  def dummy_entity_name_set(event)
+    DummyEntitySubscriber.event_name = event.name
+  end
+end
+
+describe "end to end" do
+  before(:each) do
+    MongoEntityStore.connection_profile = "mongodb://localhost/entity_store_test"
+
+    EntityStore::Config.setup do |config|
+      config.store = MongoEntityStore.new
+      config.event_subscribers << DummyEntitySubscriber
+    end
+  end
+
+  context "when save entity" do
+    let(:name) { random_string }
+    subject {
+      entity = DummyEntity.new
+      entity.set_name name
+      Store.new.save entity
+    }
+    it "publishes event to the subscriber" do
+      subject
+      DummyEntitySubscriber.event_name.should eq(name)
+    end
+  end
+end
